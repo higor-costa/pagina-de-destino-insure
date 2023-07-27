f6f583349f882eccec06908facc025a73108e04c
Higor Costa <higorcosta972@gmail.com>
2023-07-27 09:07:46 -0300

    Adiciona itens do menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,27 @@ import { ReactComponent as IconeHamburger } from '../img/icon-hamburger.svg';
 import { ReactComponent as IconeClose } from '../img/icon-close.svg';
 
 const Header = () => {
-  return <header></header>;
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">How We Work</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Account</a>
+          </li>
+          <li>
+            <a href="#">View Plans</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
