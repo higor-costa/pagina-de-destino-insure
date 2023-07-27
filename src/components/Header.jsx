b253d30f8319b7d182712c25789bcf61b7ac3e11
Higor Costa <higorcosta972@gmail.com>
2023-07-27 08:55:12 -0300

    Importa svg: logo, hamburger e close

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../img/logo.svg';
+import { ReactComponent as IconeHamburger } from '../img/icon-hamburger.svg';
+import { ReactComponent as IconeClose } from '../img/icon-close.svg';
 
 const Header = () => {
   return <header></header>;
