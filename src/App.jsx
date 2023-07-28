43ba347b33d59c9ed496b4416b80d8e7669ec5c0
Higor Costa <higorcosta972@gmail.com>
2023-07-28 08:31:30 -0300

    Importa Header no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './style/components/App.scss';
+import Header from './components/Header';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
