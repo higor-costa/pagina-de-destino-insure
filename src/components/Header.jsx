5193b4b9ce4d66307c96212138f26bcc6f729db8
Higor Costa <higorcosta972@gmail.com>
2023-07-28 08:28:26 -0300

    Define nome de classes para as tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { ReactComponent as Logo } from '../img/logo.svg';
 import { ReactComponent as IconeHamburger } from '../img/icon-hamburger.svg';
 import { ReactComponent as IconeClose } from '../img/icon-close.svg';
 import imageIntro from '../img/image-intro-mobile.jpg';
-import patternIntroLeft from '../img/bg-pattern-intro-left-mobile.svg'
-import patternIntroRight from '../img/bg-pattern-intro-right-mobile.svg'
-import patternNav from '../img/bg-pattern-mobile-nav.svg'
+import patternIntroLeft from '../img/bg-pattern-intro-left-mobile.svg';
+import patternIntroRight from '../img/bg-pattern-intro-right-mobile.svg';
+import patternNav from '../img/bg-pattern-mobile-nav.svg';
 import useMedia from '../hooks/useMedia';
 import styles from '../style/components/Header.module.scss';
 
@@ -14,51 +14,71 @@ const Header = () => {
   const [mobileMenu, setMobileMenu] = React.useState(false);
 
   return (
-    <header>
-      <nav>
-        <Logo />
-        {mobile && (
-          <button
-            onClick={() => setMobileMenu(!mobileMenu)}
-            aria-label="menu"
-            className={`${styles.botaoMenu}`}
-          >
-            {mobileMenu ? <IconeClose /> : <IconeHamburger />}
-          </button>
-        )}
-        <ul>
-          <li>
-            <a href="#">How We Work</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Account</a>
-          </li>
-          <li>
-            <a href="#">View Plans</a>
-          </li>
-          <img src={patternNav} alt="Illustration" />
-        </ul>
-      </nav>
+    <header className={styles.cabecalho}>
+      <div className={styles.container}>
+        <nav>
+          <Logo className={styles.logo} />
+          {mobile && (
+            <button
+              onClick={() => setMobileMenu(!mobileMenu)}
+              aria-label="menu"
+              className={`${styles.buttonMenu}`}
+            >
+              {mobileMenu ? <IconeClose /> : <IconeHamburger />}
+            </button>
+          )}
+          <ul className={styles.menu}>
+            <li>
+              <a href="#">How We Work</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Account</a>
+            </li>
+            <li>
+              <a href="#">View Plans</a>
+            </li>
+            <img
+              src={patternNav}
+              alt="Illustration"
+              className={styles.illustration}
+            />
+          </ul>
+        </nav>
+      </div>
 
-      <main>
-        <div>
-          <div>
-            {mobile && <img src={patternIntroLeft} alt="Left Illustration" />}
-            <h1>
+      <main className={styles.mainContent}>
+        <div className={styles.container}>
+          <div className={styles.text}>
+            {mobile && (
+              <img
+                src={patternIntroLeft}
+                alt="Left Illustration"
+                className={styles.illustrationLeft}
+              />
+            )}
+            <h1 className={styles.title}>
               Humanizing <br /> your insurance.
             </h1>
-            <p>
+            <p className={styles.paragraph}>
               Get your life insurance coverage easier and faster. We blend our
-              expertise and technology to help you find the plan that´s right for
-              you. Ensure you and loved ones are protected.
+              expertise and technology to help you find the plan that´s right
+              for you. Ensure you and loved ones are protected.
             </p>
-            <a href="#">View Plans</a>
-            {mobile && <img src={patternIntroRight} alt="Right Illustration" />}
+            <a href="#" className={styles.link}>
+              View Plans
+            </a>
+            {mobile && (
+              <img
+                src={patternIntroRight}
+                alt="Right Illustration"
+                className={styles.illustrationRight}
+              />
+            )}
           </div>
-          <div>
+          <div className={styles.image}>
             <img src={imageIntro} alt="Family photo" />
           </div>
         </div>
