3f89b28fe192b41364e390c289a8e65d20b15f96
Higor Costa <higorcosta972@gmail.com>
2023-08-01 08:21:19 -0300

    Importa ilustração 'How we work'

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactComponent as SnappyProcess } from '../img/icon-snappy-process.svg';
 import { ReactComponent as AffordablePrices } from '../img/icon-affordable-prices.svg';
 import { ReactComponent as PeopleFirst } from '../img/icon-people-first.svg';
+import illustrationPattern from '../img/bg-pattern-how-we-work-mobile.svg';
 
 const Main = () => {
   return (
