0bac00c494b9bb6a07b3637bc2d6129c2f0882c7
Higor Costa <higorcosta972@gmail.com>
2023-08-01 08:20:03 -0300

    Importa imagens dos cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ReactComponent as SnappyProcess } from '../img/icon-snappy-process.svg';
+import { ReactComponent as AffordablePrices } from '../img/icon-affordable-prices.svg';
+import { ReactComponent as PeopleFirst } from '../img/icon-people-first.svg';
 
 const Main = () => {
   return (
