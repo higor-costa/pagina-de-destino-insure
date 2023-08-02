437256a08ea64039fb13df4b393fdf0665fe6905
Higor Costa <higorcosta972@gmail.com>
2023-08-02 08:25:49 -0300

    Importa componente Footer.jsx no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './style/components/App.scss';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
