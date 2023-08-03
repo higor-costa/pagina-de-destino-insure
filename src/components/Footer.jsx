7034987a94831d06f098c06901127ca049e9bc6d
Higor Costa <higorcosta972@gmail.com>
2023-08-03 07:37:53 -0300

    Importa imagens do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styles from '../style/components/Footer.module.scss';
+import { ReactComponent as Logo } from '../img/logo.svg';
+import { ReactComponent as Facebook } from '../img/icon-facebook.svg';
+import { ReactComponent as Twitter } from '../img/icon-twitter.svg';
+import { ReactComponent as Pinterest } from '../img/icon-pinterest.svg';
+import { ReactComponent as Instagram } from '../img/icon-instagram.svg';
+import illustration from '../img/bg-pattern-footer-mobile.svg';
 
 const Footer = () => {
   return (
